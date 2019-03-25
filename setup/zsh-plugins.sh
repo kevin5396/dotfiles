6d36aa4d0bf47daf7ddd1723ef6a732d68b46061
Kangwei Ling <lingkangwei.kevin@gmail.com>
2019-03-25 14:50:13 -0500

    add zsh plugin setup scripts

---
+++ setup/zsh-plugins.sh
@@ -0,0 +1,4 @@
+
+
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
